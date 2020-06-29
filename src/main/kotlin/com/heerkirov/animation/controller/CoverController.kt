899f9b3fdf364f9bea34275da472ab7a57ace544
Heer Kirov <HeerKirov@outlook.com>
2020-06-29 20:48:47 +0800

    修正封面API相关的bug

--- src/main/kotlin/com/heerkirov/animation/controller/CoverController.kt
+++ src/main/kotlin/com/heerkirov/animation/controller/CoverController.kt
@@ -5,12 +5,14 @@ import com.heerkirov.animation.service.CoverService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.view.RedirectView
 
-@RestController("/api/database/cover")
+@RestController
+@RequestMapping("/api/database/cover")
 class CoverController(@Autowired private val coverService: CoverService) {
-    @GetMapping("/{type}/{file}")
+    @GetMapping("/{type}/{file:.+}")
     fun get(@PathVariable type: String, @PathVariable file: String): Any {
         val coverType = CoverType.valueOf(type.toUpperCase())
         return RedirectView(coverService.getCoverURL(coverType, file).toString())
